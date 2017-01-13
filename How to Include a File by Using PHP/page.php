462456e7a2b3ec6110f47c0543df882ee7e13b13
almanzanoex <alexmanzano.personal@gmail.com>
2017-01-13 17:03:40 -0600

    How to Include a File
    How to Include a File Using PHP

---
+++ How to Include a File by Using PHP/page.php
@@ -0,0 +1,11 @@
+<!DOCTYPE>
+<html>
+  <head>
+    <title>How to Include a File Using PHP</title>
+  </head>
+  <body>
+    <?php
+      include "includes/hello-world.php";
+    ?>
+  </body>
+</html>
