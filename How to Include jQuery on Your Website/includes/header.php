a20bb2f2993a0a3d69237e9d91548d01ad4af475
almanzanoex <alexmanzano.personal@gmail.com>
2017-01-13 17:17:28 -0600

    How to Include jQuery
    How to Include jQuery on Your Website

---
+++ How to Include jQuery on Your Website/includes/header.php
@@ -0,0 +1,2 @@
+<!--jQuery Plugin-->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
---
+++ How to Include jQuery on Your Website/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE>
+<html>
+  <head>
+    <title>How to Include jQuery on Your Website</title>
+    <?php
+      include "includes/header.php";
+    ?>
+  </head>
+  <body>
+  </body>
+  <script>
+    $(document).ready(function() {
+      alert("gello");
+    });
+  </script>
+</html>
