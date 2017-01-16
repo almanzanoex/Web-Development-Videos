0038d465ef133855fec6a302ce5d4d131345069b
almanzanoex <alexmanzano.personal@gmail.com>
2017-01-16 13:59:44 -0800

    Remove Space between Inline Block Elements
    How to Remove Space between Inline Block Elements Using HTML Comments

---
+++ How to Remove Space between Inline Block Elements Using HTML Comments/index.php
@@ -0,0 +1,11 @@
+<!--CSS Styling-->
+<link rel="stylesheet" href="css/index.css"/>
+
+<!--Container-->
+<div id="container">
+  <!--Inline Block Elements-->
+  <div class="sections" id="sec1">section 1</div><!--
+  --><div class="sections" id="sec2">section 2</div><!--
+  --><div class="sections" id="sec3">section 3</div><!--
+  --><div class="sections" id="sec4">section 4</div>
+</div>
