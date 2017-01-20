b37ed2c6cf6fd4ae5718459b812d0bac7939c731
almanzanoex <alexmanzano.personal@gmail.com>
2017-01-20 13:19:46 -0800

    How to Link a JavaScript File
    How to Link a JavaScript File

---
+++ How to Link a JavaScript File/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE>
+<html>
+  <head>
+    <title>How to Link in a JavaScript File</title>
+    <link rel="stylesheet" href="css/index.css"/>
+  </head>
+  <body>
+    <div class="box"></div>
+  </body>
+  <script src="js/index.js" type="text/javascript"></script>
+</html>
