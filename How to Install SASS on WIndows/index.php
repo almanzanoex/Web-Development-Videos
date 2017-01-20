789b5ca65ca07576e8795310d27ac8f0dbdfb6df
almanzanoex <alexmanzano.personal@gmail.com>
2017-01-20 13:04:36 -0800

    How to Install SASS on Windows
    How to Install SASS on Windows

---
+++ How to Install SASS on WIndows/index.php
@@ -0,0 +1,10 @@
+<!--SASS Stylesheet-->
+<link rel="stylesheet" href="css/index1.css"/>
+
+<!--
+  SCSS Stylesheet
+  <link rel="stylesheet" href="css/index1.css"/>
+-->
+
+<!--The element that is being styled-->
+<div class="sass-box"></div>
