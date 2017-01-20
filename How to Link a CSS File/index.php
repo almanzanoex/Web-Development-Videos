4f1949c33982ac955e159629115e63765adeee57
almanzanoex <alexmanzano.personal@gmail.com>
2017-01-20 13:12:35 -0800

    How to Link a CSS File
    How to Link a CSS File

---
+++ How to Link a CSS File/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE>
+<html>
+  <head>
+    <title>How to Link in a CSS File</title>
+    <link rel="stylesheet" href="css/index.css"/>
+  </head>
+  <body>
+    <div class="box"></div>
+  </body>
+</html>
